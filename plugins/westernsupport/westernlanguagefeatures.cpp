2ddee5471d89dc33e79a5d7edfc62a578fa77bff
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2014-03-07 22:15:54 +0100

    Some more test changes

--- plugins/westernsupport/westernlanguagefeatures.cpp
+++ plugins/westernsupport/westernlanguagefeatures.cpp
@@ -44,7 +44,7 @@ WesternLanguageFeatures::~WesternLanguageFeatures()
 
 bool WesternLanguageFeatures::activateAutoCaps(const QString &preedit) const
 {
-    static const QString sentenceBreak = QString::fromUtf8("!.?:\n");
+    static const QString sentenceBreak = QString::fromUtf8("!.?:\n\r");
 
     if (preedit.isEmpty()) {
         return false;
