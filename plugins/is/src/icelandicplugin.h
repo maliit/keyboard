07a6276c4adb5ed0b1d6b741f9374eb22adf514f
Michael Sheldon <michael.sheldon@canonical.com>
2015-05-11 12:06:09 +0100

    Add source and word database for Icelandic plugin

---
+++ plugins/is/src/icelandicplugin.h
@@ -0,0 +1,25 @@
+﻿#ifndef ICELANDICPLUGIN_H
+#define ICELANDICPLUGIN_H
+
+#include <QObject>
+#include "languageplugininterface.h"
+#include "westernlanguagesplugin.h"
+
+class IcelandicPlugin : public WesternLanguagesPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(LanguagePluginInterface)
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.Examples.IcelandicPlugin" FILE "icelandicplugin.json")
+
+public:
+    explicit IcelandicPlugin(QObject* parent = 0)
+        : WesternLanguagesPlugin(parent)
+    {
+    }
+
+    virtual ~IcelandicPlugin()
+    {
+    }
+};
+
+#endif // ICELANDICPLUGIN_H
