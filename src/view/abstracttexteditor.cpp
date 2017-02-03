dd94ee0c41033012b9f8de99798221c7f093a5dd
Michael Sheldon <michael.sheldon@canonical.com>
2017-02-03 13:39:43 +0000

    Disable spaces on word ribbon selection, but keep them when pressing the space bar

--- src/view/abstracttexteditor.cpp
+++ src/view/abstracttexteditor.cpp
@@ -501,8 +501,8 @@ void AbstractTextEditor::onKeyReleased(const Key &key)
         }
 
         if (replace_preedit) {
-            if ((!textOnRight.isEmpty() && d->editing_middle_of_text) || d->word_engine->languageFeature()->contentType() == Maliit::UrlContentType) {
-                // Don't insert a space if we are correcting a word in the middle of a sentence or if we're in URL mode
+            if (!textOnRight.isEmpty() && d->editing_middle_of_text) {
+                // Don't insert a space if we are correcting a word in the middle of a sentence
                 space = "";
                 d->look_for_a_double_space = false;
                 d->editing_middle_of_text = false;
@@ -733,8 +733,8 @@ void AbstractTextEditor::replaceAndCommitPreedit(const QString &replacement)
     const bool auto_caps_activated = d->word_engine->languageFeature()->activateAutoCaps(d->text->preedit());
     d->appendix_for_previous_preedit = d->word_engine->languageFeature()->appendixForReplacedPreedit(d->text->preedit());
     if (d->auto_correct_enabled) {
-        if (!d->text->surroundingRight().trimmed().isEmpty() && d->editing_middle_of_text) {
-            // Don't insert a space if we are correcting a word in the middle of a sentence
+        if ((!d->text->surroundingRight().trimmed().isEmpty() && d->editing_middle_of_text) || d->word_engine->languageFeature()->contentType() == Maliit::UrlContentType) {
+            // Don't insert a space if we are correcting a word in the middle of a sentence or if we're in a Url field
             d->appendix_for_previous_preedit = "";
             d->editing_middle_of_text = false;
         }
