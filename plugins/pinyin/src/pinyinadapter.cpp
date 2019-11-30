269646fd87ef79118493011fae3a3c30ac395586
Jan Arne Petersen <janarne@gmail.com>
2019-11-30 11:28:35 +0100

    [pinyin] Add additional required paramter

--- plugins/pinyin/src/pinyinadapter.cpp
+++ plugins/pinyin/src/pinyinadapter.cpp
@@ -58,7 +58,7 @@ void PinyinAdapter::parse(const QString& string)
     std::cout << std::endl;
 #endif
 
-    pinyin_guess_candidates(m_instance, 0);
+    pinyin_guess_candidates(m_instance, 0, SORT_BY_PHRASE_LENGTH_AND_PINYIN_LENGTH_AND_FREQUENCY);
 
     candidates.clear();
     guint len = 0;
