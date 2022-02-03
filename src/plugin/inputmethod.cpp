maliit/keyboard

commit fe7ff7ad07b1faeb880037edd91bcafe4ab4878e

Allow disabling show/hide animations with an environment variable
Currently, the animation for showing or hiding the keyboard is done
client side. This leads to some nasty race conditions, at least on KWin
Wayland, because the keyboard rect is sent asynchronously and any window
geometry changes it causes are also async.

To allow the animation to be done by the compositor directly, we first
need to disable the client-side animation.

src/plugin/inputmethod.cpp
@@ -134,6 +134,12 @@ InputMethod::InputMethod(MAbstractInputMethodHost *host)
     // settings to be initialized first:
     d->setLayoutOrientation(d->appsCurrentOrientation);
 
+    // If MALIIT_ENABLE_ANIMATIONS environment is set and 0, disable animations,
+    // otherwise enable them.
+    bool animationOk = false;
+    int animationEnv = qEnvironmentVariableIntValue("MALIIT_ENABLE_ANIMATIONS", &animationOk);
+    d->animationEnabled = animationOk && animationEnv != 0;
+
     QString prefix = qgetenv("KEYBOARD_PREFIX_PATH");
     if (!prefix.isEmpty()) {
         d->view->setSource(QUrl::fromLocalFile(prefix + QDir::separator() + g_maliit_keyboard_qml));
@@ -739,6 +745,12 @@ QString InputMethod::surroundingRight()
 {
     Q_D(InputMethod);
     return d->editor.text()->surroundingRight();
+}
+
+bool InputMethod::isAnimationEnabled() const
+{
+    Q_D(InputMethod);
+    return d->animationEnabled;
 }
 
 bool InputMethod::languageIsSupported(const QString plugin) {

src/plugin/inputmethod.h
@@ -65,6 +65,7 @@ class InputMethod
     Q_PROPERTY(QString theme READ theme NOTIFY themeChanged)
     Q_PROPERTY(QString surroundingLeft READ surroundingLeft)
     Q_PROPERTY(QString surroundingRight READ surroundingRight)
+    Q_PROPERTY(bool animationEnabled READ isAnimationEnabled CONSTANT)
 
 public:
     /// Same as Maliit::TextContentType but usable in QML
@@ -143,6 +144,8 @@ public:
 
     QString surroundingLeft();
     QString surroundingRight();
+
+    bool isAnimationEnabled() const;
 
     Q_SLOT void close();
 

qml/Keyboard.qml
@@ -219,7 +219,10 @@ Item {
 
         PropertyAnimation {
             id: bounceBackAnimation
-            target: keyboardSurface
+            // Animations don't have an "enabled" property, so just set the
+            // target to null if animation is disabled, which effectively also
+            // disables the animation.
+            target: maliit_input_method.animationEnabled ? keyboardSurface : null
             properties: "y"
             easing.type: Easing.OutBounce;
             easing.overshoot: 2.0
@@ -267,6 +270,7 @@ Item {
             }
         ]
         transitions: Transition {
+            enabled: maliit_input_method.animationEnabled
             NumberAnimation { target: keyboardSurface; properties: "y"; duration: 165}
         }
 

src/plugin/inputmethod_p.h
@@ -107,6 +107,8 @@ public:
     QStringList languagesPaths;
     QString currentPluginPath;
 
+    bool animationEnabled = true;
+
     explicit InputMethodPrivate(InputMethod * const _q,
                                 MAbstractInputMethodHost *host)
         : q(_q)

commit b0d5d5cdb8f1b84d78c12be81cc8f39dea1092bc

Fix build failure introduced by fe7ff7ad07b1faeb880037edd91bcafe4ab4878e
Q_D() doesn't work inside const methods.

src/plugin/inputmethod.cpp
@@ -747,7 +747,7 @@ QString InputMethod::surroundingRight()
     return d->editor.text()->surroundingRight();
 }
 
-bool InputMethod::isAnimationEnabled() const
+bool InputMethod::isAnimationEnabled()
 {
     Q_D(InputMethod);
     return d->animationEnabled;

src/plugin/inputmethod.h
@@ -65,7 +65,7 @@ class InputMethod
     Q_PROPERTY(QString theme READ theme NOTIFY themeChanged)
     Q_PROPERTY(QString surroundingLeft READ surroundingLeft)
     Q_PROPERTY(QString surroundingRight READ surroundingRight)
-    Q_PROPERTY(bool animationEnabled READ isAnimationEnabled CONSTANT)
+    Q_PROPERTY(bool animationEnabled READ isAnimationEnabled)
 
 public:
     /// Same as Maliit::TextContentType but usable in QML
@@ -145,7 +145,7 @@ public:
     QString surroundingLeft();
     QString surroundingRight();
 
-    bool isAnimationEnabled() const;
+    bool isAnimationEnabled();
 
     Q_SLOT void close();
 